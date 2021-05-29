dea5761764b4fe32c28077a10940d4bb443db7ae
williamhelmrath <wph2tu@virginia.edu>
2021-05-29 17:55:24 -0400

    test authentication and search in client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi</div>;
+  const [token, setToken] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("/authenticate")
+      .then((res) => res.text())
+      .then((res) => setToken(res));
+  }, []);
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=${searchTerm}&type=track`,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then((res) => res.json());
+
+    console.log(res);
+  };
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {token}
+    </div>
+  );
 }
 
 export default App;
