7751142a0f38a2ac6e900839e21fdebdd83786a4
williamhelmrath <wph2tu@virginia.edu>
2021-05-30 00:39:17 -0400

    fix build path

--- server/app.ts
+++ server/app.ts
@@ -75,9 +75,9 @@ app.get("/authenticate", async (req, res) => {
   res.send(access_token);
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(PORT, () => {
